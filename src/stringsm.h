e9455dec5fb95a362109d8247434fc387625283a
adrien <51877882+Poto-Feu@users.noreply.github.com>
2020-04-15 19:45:28 +0200

    Add: stringsm_ext_str_quotes func

--- src/stringsm.h
+++ src/stringsm.h
@@ -24,5 +24,6 @@ void stringsm_chomp(char* str);
 void stringsm_rtab(char* str);
 void stringsm_getuseri(char** buf);
 void stringsm_getfw(char** fw, char* s, int *index);
+void stringsm_ext_str_quotes(char** r_ext, char* p_str);
 
 #endif
