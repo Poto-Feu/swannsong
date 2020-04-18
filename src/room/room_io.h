840252e56a96d85fb70f02147a3b09c1a9a2ac68
adrien <51877882+Poto-Feu@users.noreply.github.com>
2020-04-18 15:59:28 +0200

    Add: room_io files

---
+++ src/room/room_io.h
@@ -0,0 +1,33 @@
+/*
+    Copyright (C) 2020 Adrien Saad
+
+    This file is part of SwannSong.
+
+    SwannSong is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SwannSong is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SwannSong.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef ROOM_IO_H
+#define ROOM_IO_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
