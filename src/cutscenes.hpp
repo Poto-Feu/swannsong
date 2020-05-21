b9ec1a3f6b70f10d810030b0fa4685ce1eb55d39
adrien <51877882+Poto-Feu@users.noreply.github.com>
2020-05-21 21:55:58 +0200

    Add: create cutscenes module

---
+++ src/cutscenes.hpp
@@ -0,0 +1,53 @@
+/*
+    Copyright (C) 2020 Adrien Saad
+
+    This file is part of SwannSong.
+
+    SwannSong is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SwannSong is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SwannSong.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef CUTSCENES_HPP
+#define CUTSCENES_HPP
+
+#include <string>
+#include <vector>
+
+enum cs_action_type
+{
+    STRING,
+    PAUSE
+};
+
+struct cs_action
+{
+    cs_action_type type;
+    std::string content;
+};
+
+class Cutscene
+{
+    public:
+
+    Cutscene();
+
+    std::string name;
+    std::vector<cs_action> vec;
+};
+
+namespace cutscenes
+{
+    void copy_file_to_vec();
+    void display(std::string const p_name);
+}
+#endif
