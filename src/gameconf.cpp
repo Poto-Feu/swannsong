6be686884768defb2049b185bcb155301ee8f953
adrien <51877882+Poto-Feu@users.noreply.github.com>
2021-10-01 23:00:21 +0200

    Fix gameconf.cpp formatting

--- src/gameconf.cpp
+++ src/gameconf.cpp
@@ -14,8 +14,8 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with SwannSong Adventure.
-    If not, see <https://www.gnu.org/licenses/>.
+    along with SwannSong Adventure.  If not, see
+    <https://www.gnu.org/licenses/>.
 */
 
 #include <fstream>
@@ -25,37 +25,36 @@
 #include "game_error.hpp"
 #include "stringsm.hpp"
 
-namespace gameconf
+/* Read data contained in the gameconf file and set the gameconf variable to
+ * the appropriate value */
+std::vector<gameconf::gcvar_struct> gameconf::readfile(
+        std::string const& data_path)
 {
-    /*Read data contained in the gameconf file and set the gameconf variable to the appropriate
-    value*/
-    std::vector<gcvar_struct> readfile(std::string const& data_path)
-    {
-        using namespace game_error;
+    std::ifstream gc_stream(data_path + "gameconf.txt");
 
-        std::ifstream gc_stream(data_path + "gameconf.txt");
-
-        if(!gc_stream.good()) {
-            fatal_error("gameconf file not found (this may also applies to other game files)");
-        }
-
-        std::vector<gcvar_struct> rtrn_vec;
-        std::string curr_line;
-
-        while(std::getline(gc_stream, curr_line)) {
-            stringsm::rtab(curr_line);
-            if(curr_line.empty()) continue;
-            if(curr_line.at(0) != '#')
-            {
-                std::string var;
-                std::string value;
-
-                if(conf_files::split_var(var, value, curr_line)) {
-                    rtrn_vec.push_back(gcvar_struct {var, value});
-                } else emit_warning("incorrect gameconf syntax");
-            }
-            curr_line.clear();
-        }
-        return rtrn_vec;
+    if(!gc_stream.good()) {
+        game_error::fatal_error("gameconf file not found (this may also "
+                "applies to other game files)");
     }
+
+    std::vector<gcvar_struct> rtrn_vec;
+    std::string curr_line;
+
+    while(std::getline(gc_stream, curr_line)) {
+        stringsm::rtab(curr_line);
+        if(curr_line.empty()) continue;
+        if(curr_line.at(0) != '#')
+        {
+            std::string var;
+            std::string value;
+
+            if(conf_files::split_var(var, value, curr_line)) {
+                rtrn_vec.push_back(gcvar_struct {var, value});
+            } else {
+                game_error::emit_warning("incorrect gameconf syntax");
+            }
+        }
+        curr_line.clear();
+    }
+    return rtrn_vec;
 }
